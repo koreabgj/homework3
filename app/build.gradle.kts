5b85b8831b56d1418e0612c018ad201d4b734eb2
koreabgj <ligerbae@gmail.com>
2024-03-22 17:55:22 +0900

    레벨3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.ads:mediation-test-suite:3.0.0")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.6.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/mission3/HomeActivity.kt
+++ app/src/main/java/com/example/mission3/HomeActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mission3
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
+import android.view.animation.AnimationUtils
 
 class HomeActivity : Activity()
 
@@ -20,20 +24,24 @@ class HomeActivityActivity : AppCompatActivity() {
         image1.setImageResource(R.drawable.koreabgj)
 
         val text1: TextView = findViewById(R.id.name)
+        val text2: TextView = findViewById(R.id.wording)
 
+        val text3: TextView = findViewById(R.id.true_false)
+        // 애니메이션 리소스 가져오기
+        val animation = AnimationUtils.loadAnimation(this, R.anim.text_effect)
+        // 텍스트뷰에 애니메이션 적용
+        text3.startAnimation(animation)
 
+        val text4: TextView = findViewById(R.id.mbti)
 
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://lionbae.tistory.com")))
+        }
+        button1.paintFlags = text3.paintFlags or Paint.UNDERLINE_TEXT_FLAG
 
-
-
-        val text2: TextView = findViewById(R.id.mbti)
-
-        val text3: TextView = findViewById(R.id.blogTextView)
-
-        val button: Button = findViewById(R.id.button)
-
-        button.setOnClickListener {
-            // 종료 버튼이 눌리면 SignInActivity로 이동
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
             finish()
         }
     }
