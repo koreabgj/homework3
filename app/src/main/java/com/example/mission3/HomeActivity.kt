a8cdf2f79b8fdb742230d302a3ad805cddf9b489
koreabgj <ligerbae@gmail.com>
2024-03-22 14:57:07 +0900

    '

--- app/src/main/java/com/example/mission3/HomeActivity.kt
+++ app/src/main/java/com/example/mission3/HomeActivity.kt
@@ -1,25 +1,34 @@
 package com.example.mission3
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : Activity()
 
 class HomeActivityActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_home)
 
-        val imageView: ImageView = findViewById(R.id.koreabgj)
-        imageView.setImageResource(R.drawable.koreabgj)
+        val image1: ImageView = findViewById(R.id.koreabgj)
+        image1.setImageResource(R.drawable.koreabgj)
 
-
-
-
+        val text1: TextView = findViewById(R.id.name)
 
 
+
+
+
+
+        val text2: TextView = findViewById(R.id.mbti)
+
+        val text3: TextView = findViewById(R.id.blogTextView)
 
         val button: Button = findViewById(R.id.button)
 
