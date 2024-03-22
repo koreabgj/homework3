b59b4442e622c7723c097ea94b3ac42b9f695557
koreabgj <ligerbae@gmail.com>
2024-03-22 14:19:13 +0900

    ,

--- app/src/main/java/com/example/mission3/HomeActivity.kt
+++ app/src/main/java/com/example/mission3/HomeActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mission3
-
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : Activity()
+
+class HomeActivityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_sign_up)
+
+        val imageView: ImageView = findViewById(R.id.koreabgj)
+        imageView.setImageResource(R.drawable.koreabgj)
+
+
+
+
+
+
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            // 종료 버튼이 눌리면 SignInActivity로 이동
+            finish()
+        }
     }
 }
